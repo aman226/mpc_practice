84b253094ee65a33819493058785f73a0ce2fca3
Aman Kumar Singh <amanmain3@gmail.com>
2023-01-24 09:15:50 +0530

    Update frameviz.py

--- mpc_drone/visualizer/frameviz.py
+++ mpc_drone/visualizer/frameviz.py
@@ -6,42 +6,59 @@ class FrameVisualization():
     def __init__(self, xlim = [0, 10], ylim = [0, 10], zlim = [0, 10], alpha_world_axis = 1) -> None:
         self.fig = plt.figure()
         self.ax = plt.axes(projection='3d')
-        self.frames = {}
+
         self.setScale(xlim,ylim,zlim)
-
-        
-
-        origin = np.array([0,0,0])
+        origin = np.array([0.0,0.0,0.0])
         origin_axes = np.array([ [1.0 , 0.0, 0.0], 
-                                 [0.0 , 1.0, 0.0], 
-                                 [0.0 , 0.0, 1.0]])
-        
-        self.addFrame("World", origin, origin_axes, alpha_world_axis)
+                          [0.0 , 1.0, 0.0], 
+                          [0.0 , 0.0, 1.0]])
 
+        self.frames_for_transformation = {}
+        self.frames_for_current_ref = {}
+        
+        self.frames_for_current_ref["World"] = [origin, origin_axes, alpha_world_axis]
+        self.frames_for_transformation["World"] = [origin, origin_axes, alpha_world_axis]
+
+        
+        self.currentFrame = "World"
 
     ############################################################################################################################
     
-    def addFrame(self,frameName, position, basisVectors, alpha = 1.0, overwrite = False):
+    def addFrame(self, frameName, position, basisVectors, alpha = 1.0, overwrite = False):
         if not overwrite:
-            if frameName in self.frames:
+            if frameName in self.frames_for_transformation:
                 print("Error, Frame with same Name Exists! If you want to overwrite, set overwriting to True while calling this method")
                 return
-        self.frames[frameName] = [position, basisVectors, alpha]
+        # Keep Track of transformations with respect to current reference frame
+        self.frames_for_current_ref[frameName] = [position, basisVectors, alpha]
+
+        # Keep Track of transformations with respect to World Frameransformation[frameName] 
+        self.frames_for_transformation[frameName] = [position - self.frames_for_transformation[self.currentFrame][0], self.frames_for_transformation[self.currentFrame][1].T @ basisVectors, alpha]
+        print(self.frames_for_transformation)
+
 
     def removeFrame(self,frameName):
-        if frameName in self.frames:
-            self.frames.pop(frameName)
+        if frameName in self.frames_for_transformation:
+
+            # Pop Frames 
+            self.frames_for_current_ref.pop(frameName)
+            self.frames_for_transformation.pop(frameName)
+
             return
         print("Error, Frame Not Found")
 
     def displayFrame(self, frameName, pauseTime = 0.0, clear = False):
+        """
+        Display given frame with respect to current reference frame.
+        """
         if clear:
             self.ax.clear()
             self.setScale(self.xlim,self.ylim,self.zlim)
-        if frameName in self.frames:
-            position = self.frames[frameName][0]
-            basisVectors = self.frames[frameName][1]
-            alpha = self.frames[frameName][2]
+
+        if frameName in self.frames_for_current_ref:
+            position = self.frames_for_current_ref[frameName][0]
+            basisVectors = self.frames_for_current_ref[frameName][1]
+            alpha = self.frames_for_current_ref[frameName][2]
             self.ax.quiver(position[0], position[1], position[2], basisVectors[0,0], basisVectors[1,0], basisVectors[2,0],color="red",alpha = alpha)
             self.ax.quiver(position[0], position[1], position[2], basisVectors[0,1], basisVectors[1,1], basisVectors[2,1],color="green",alpha = alpha)
             self.ax.quiver(position[0], position[1], position[2], basisVectors[0,2], basisVectors[1,2], basisVectors[2,2],color="blue",alpha = alpha)
@@ -51,68 +68,84 @@ class FrameVisualization():
             print("Frame Does Not Exist")
     
     def displayAllFrames(self, pauseTime = 0.0):
+        """
+        Display All Frames with respect to current reference frame.
+        """
         self.ax.clear()
         self.setScale(self.xlim,self.ylim,self.zlim)
-        for frame in self.frames:
-            position = self.frames[frame][0]
-            basisVectors = self.frames[frame][1]
-            alpha = self.frames[frame][2]
+        for frame in self.frames_for_current_ref:
+            position = self.frames_for_current_ref[frame][0]
+            basisVectors = self.frames_for_current_ref[frame][1]
+            alpha = self.frames_for_current_ref[frame][2]
             self.ax.quiver(position[0], position[1], position[2], basisVectors[0,0], basisVectors[1,0], basisVectors[2,0],color="red",alpha = alpha)
             self.ax.quiver(position[0], position[1], position[2], basisVectors[0,1], basisVectors[1,1], basisVectors[2,1],color="green",alpha = alpha)
             self.ax.quiver(position[0], position[1], position[2], basisVectors[0,2], basisVectors[1,2], basisVectors[2,2],color="blue",alpha = alpha)
         if pauseTime:
                 plt.pause(pauseTime)
+        else:
+            plt.show()
     
     ############################################################################################################################
     
-    def updateFramePosition(self, frameName, Position):
-        if frameName in self.frames:
-            self.frames[frameName][0] = Position
+    def updateFramePosition(self, frameName, position):
+        if frameName in self.frames_for_transformation:
+            self.frames_for_current_ref[frameName][0] = position
+            self.frames_for_transformation[frameName][0] = position - self.frames_for_transformation[self.currentFrame][0]
+
         else:
             print("Frame Does Not Exist")
     
-    def updateBasisVector(self, frameName, basisVectors):
-        if frameName in self.frames:
-            self.frames[frameName][1] = basisVectors
+    def updateBasisVectors(self, frameName, basisVectors):
+        if frameName in self.frames_for_transformation:
+            self.frames_for_current_ref[frameName][1] = basisVectors
+            self.frames_for_transformation[frameName][1] = self.frames_for_transformation[self.currentFrame][1].T @ basisVectors
         else:
             print("Frame Does Not Exist")
     
     def updateAlpha(self, frameName, alpha):
-        if frameName in self.frames:
-            self.frames[frameName][2] = alpha
+        if frameName in self.frames_for_transformation:
+            self.frames_for_current_ref[frameName][2] = alpha
+            self.frames_for_transformation[frameName][2] = alpha
+
         else:
             print("Frame Does Not Exist")
     
-    def updateFrame(self, frameName, Position, basisVectors, alpha = -1):
-        self.updateFramePosition(frameName, Position)
-        self.updateBasisVector(frameName, basisVectors)
+    def updateFrame(self, frameName, position, basisVectors, alpha = -1):
+        self.updateFramePosition(frameName, position)
+        self.updateBasisVectors(frameName, basisVectors)
         if not alpha==-1:
             self.updateAlpha(frameName, alpha)
 
+
     def translateFrame(self, frameName, TransVec):
-        if frameName in self.frames:
-            self.frames[frameName][0] =+ TransVec
+        if frameName in self.frames_for_current_ref:
+            self.frames_for_current_ref[frameName][0] =+ TransVec
+            self.frames_for_transformation[frameName][0] =+ (TransVec - self.frames_for_transformation[self.currentFrame][0])
         else:
             print("Frame Does Not Exist")
     
     def setRotX(self,frameName,angle):
-        if frameName in self.frames:
-            self.frames[frameName][1] = self.getRotX(angle) @ self.frames[frameName][1]
+        if frameName in self.frames_for_current_ref:
+            self.frames_for_current_ref[frameName][1] = self.getRotX(angle) @ self.frames_for_current_ref[frameName][1]
+            self.frames_for_transformation[frameName][1] = self.frames_for_transformation[self.currentFrame][1].T @ self.frames_for_current_ref[frameName][1]
         else:
             print("Frame Does Not Exist")
     
     def setRotY(self,frameName,angle):
-        if frameName in self.frames:
-            self.frames[frameName][1] = self.getRotY(angle) @ self.frames[frameName][1] 
+        if frameName in self.frames_for_current_ref:
+            self.frames_for_current_ref[frameName][1] = self.getRotY(angle) @ self.frames_for_current_ref[frameName][1]
+            self.frames_for_transformation[frameName][1] = self.frames_for_transformation[self.currentFrame][1].T @ self.frames_for_current_ref[frameName][1]
         else:
             print("Frame Does Not Exist")
     
     def setRotZ(self,frameName,angle):
-        if frameName in self.frames:
-            self.frames[frameName][1] = self.getRotZ(angle) @ self.frames[frameName][1]
+        if frameName in self.frames_for_current_ref:
+            self.frames_for_current_ref[frameName][1] = self.getRotZ(angle) @ self.frames_for_current_ref[frameName][1]
+            self.frames_for_transformation[frameName][1] = self.frames_for_transformation[self.currentFrame][1].T @ self.frames_for_current_ref[frameName][1]
         else:
             print("Frame Does Not Exist")
-    
+
+
     def setScale(self,xlim,ylim,zlim):
         self.ax.set_xlim(xlim)
         self.ax.set_ylim(ylim)
@@ -137,17 +170,19 @@ class FrameVisualization():
                          [0, 0, 1]])
     
     def getFrameNames(self):
-        return list(self.frames.keys())
+        return list(self.frames_for_transformation.keys())
+    
     
     def applyEulerRotation(self, frameName, alpha_angle, beta_angle, gamma_angle, order = "ZYX"):
-        if frameName in self.frames:
+        if frameName in self.frames_for_current_ref:
             rotationMat = self.getEulerRotMatrix(alpha_angle, beta_angle, gamma_angle, order)
             if rotationMat is None:
                 return
-            self.frames[frameName][1] = rotationMat @ self.frames[frameName][1]
+            self.frames_for_current_ref[frameName][1] = rotationMat @ self.frames_for_current_ref[frameName][1]
+            self.frames_for_transformation[frameName][1] = self.frames_for_transformation[self.currentFrame][1].T @ self.frames_for_current_ref[frameName][1]
         else:
             print("Frame Does Not Exist")
-    
+
     def getEulerRotMatrix(self,alpha_angle, beta_angle, gamma_angle, order):
         if order == "XYZ":
             rotationMat = self.getRotX(alpha_angle) @ self.getRotY(beta_angle) @ self.getRotZ(gamma_angle)
@@ -191,6 +226,14 @@ class FrameVisualization():
         
         return rotationMat
 
+    def changeFrame(self, frameName):
+        if frameName in self.frames_for_transformation:
+            self.currentFrame = frameName
+
+            for frame in self.frames_for_transformation:
+                self.frames_for_current_ref[frame] = [self.frames_for_transformation[frame][0] - self.frames_for_transformation[self.currentFrame][0],self.frames_for_transformation[self.currentFrame][1].T @ self.frames_for_transformation[frame][1],self.frames_for_transformation[frame][2]]
+        else:
+            print("Frame Does Not Exist")
         
 
     
